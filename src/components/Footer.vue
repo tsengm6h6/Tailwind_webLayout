41610f3fe788f4c7b787cb913d7d29a6e82039cb
tyttseng <tyttseng@gmail.com>
2021-06-25 22:49:08 +0800

    modify footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,12 @@
   <footer class="bg-amber-dark text-white">
     <div class="container max-w-5xxl px-4 py-5 flex xl:justify-between xl:items-center">
       <div class="flex justify-between xl:justify-start items-center">
-      <a class='mr-5' href="#">
+      <a class='mr-5 mb-2' href="#">
         <img class='w-32 h-8' src="https://raw.githubusercontent.com/hexschool/webLayoutTraining1st/master/perfume-week6/d%E2%80%99Perfumewhite%402x.png" alt="logo">
       </a>
       <p class="text-sm font-medium">© 2020. All Rights Reserved.</p>
       </div>
-      <ul class="hidden xl:flex space-x-10">
+      <ul class="hidden xl:flex xl:items-center space-x-10">
         <li class="text-xl font-normal">Product</li>
         <li class="text-xl font-normal">Blog</li>
         <li class="text-xl font-normal">Contact</li>
